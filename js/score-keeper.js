2825c83c73be56fe8b6781bed00b4bd096fdb9eb
Hazel <basile.cunuder@gmail.com>
2025-05-20 15:51:55 +0200

    faet: add consts

---
+++ js/score-keeper.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const maxScore = document.getElementById("#max-score");
+const maxGoals = document.getElementById("#max-goals");
+const teamA = document.getElementById("#teamA-score");
+const teamB = document.getElementById("#teamB-score");
+const resetScpre = document.getElementById("#reset");
+const teamAScored = document.getElementById("#plus-A");
+const teamBScored = document.getElementById("#plus-B");
+
