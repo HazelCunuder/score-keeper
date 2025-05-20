e7f72e55c83e615619ced1f9a031a43047e0567c
Hazel <basile.cunuder@gmail.com>
2025-05-20 16:21:03 +0200

    feat: add score display

--- js/score-keeper.js
+++ js/score-keeper.js
@@ -1,10 +1,18 @@
 "use strict";
 
-const maxScore = document.getElementById("#max-score");
-const maxGoals = document.getElementById("#max-goals");
-const teamA = document.getElementById("#teamA-score");
-const teamB = document.getElementById("#teamB-score");
-const resetScpre = document.getElementById("#reset");
-const teamAScored = document.getElementById("#plus-A");
-const teamBScored = document.getElementById("#plus-B");
+const maxScore = document.getElementById("max-score");
+const maxGoals = document.getElementById("max-goals");
+const teamA = document.getElementById("teamA-score");
+const teamB = document.getElementById("teamB-score");
+const resetScore = document.getElementById("reset");
+const teamAScored = document.getElementById("plus-A");
+const teamBScored = document.getElementById("plus-B");
 
+let scoreA = 0;
+let scoreB = 0;
+let winningScore = 25;
+
+teamA.textContent = `Team A: ${scoreA}`;
+teamB.textContent = `Team B: ${scoreB}`;
+maxScore.textContent = `First to ${winningScore}`;
+maxGoals.value = winningScore;
